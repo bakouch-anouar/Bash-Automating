8fd16ee43d9bc00ec9c379b59fc70c778a312a29
anouar <anouar.bakouch@usmba.ac.ma>
2023-01-06 01:52:57 +0100

    a script to automate the copy of files from source to build

---
+++ script.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+echo "welcome !"
+
+firstline=$(head -n 1 ./source/changelog.md)
+
+# the number without the markdown
+read -a splitfirstline <<< $firstline
+version=${splitfirstline[1]}
+
+echo "You are building version" $version
+
+echo "enter 1 : Yes | 0 :NO "
+read $versioncontinue
+
+if [ "$versioncontinue"=="1" ]
+then
+# copy files from source to build
+    directory="source"
+    pattern="*"
+    destination="build"
+    for file in "$directory"/"$pattern"
+    do
+      if [ "$file" == "secretinfo.md" ]
+      then
+       echo "$file not copied"
+      else 
+      cp $file $destination
+      echo "$file copied"
+      fi
+    done
+    cd /"$destination"
+    ls /"$destination"
+else 
+  echo "Please come back when you are ready"
+fi
+
+
+
+
+
+
+
